609c0e1f0f0e519fb756f3cd08023500b2a4b0d3
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2021-02-19 11:08:59 -0800

    Create main.py
    created the file and added a prototype for the unitary conversion script

---
+++ unitConverter/main.py
@@ -0,0 +1,24 @@
+coversion = {"cm": 1, "in": 2.54, "mi": 160934, "km": 100000, "kg": 1, "lb": 0.453592}
+
+while(True):
+  startUnit = input("select the input unit for conversion: ").lower()
+  
+  if(startUnit == "in" or startUnit == "mi" or startUnit == "cm" or startUnit == "km"):
+    while(True):
+      endUnit = input("select the output unit for conversion: ").lower()
+      if((endUnit == "in" or endUnit == "cm" or endUnit == "km" or endUnit == "mi") and endUnit != startUnit):
+        break
+      print("please enter a valid output unit from this list ('cm', 'in', 'km', 'mi')")
+    break
+  elif(startUnit == "kg" or startUnit == "lb"):
+    if(startUnit == "kg"):
+      endUnit = "lb"
+    else:
+      endUnit = "kg"
+    break
+  print("please enter a vaild input from this list ('cm', 'in', 'mi', 'km', 'kg', 'lb')")
+print("converting from", startUnit, "to", endUnit)
+
+input = float(input("please enter a value to convert: "))
+
+print((input * coversion[startUnit]) / coversion[endUnit], endUnit)
