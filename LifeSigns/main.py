2738c6702e5661693d18dcc483fa7d2623adb0ea
Earth13wells <gabrielw.presley@protonmail.com>
2021-02-19 16:13:20 -0800

    lifesigns working

--- LifeSigns/main.py
+++ LifeSigns/main.py
@@ -1,22 +1,30 @@
 age = int(input("What is your age (Years 0-18):"))
+age += 1
 breaths_low = breaths_high = beats = 0
-breaths_low += (525600*25)
-breaths_high += (525600*60)
-for i in range(age):
-    print(i+1)
-    if 1< i+1 <=4 :
-        breaths_low += (525600*20*i)
-        breaths_high += (525600*30*i)
-        beats += (67.5*525600)
-    if 4< i+1 <14 :
-        breaths_low += (525600*15*i)
-        breaths_high += (525600*25*i)
-        beats += (67.5*525600)
 
-    if 14< i+1 <18 :
-        breaths_low += (525600*11*i)
-        breaths_high += (525600*18*i)
+print(breaths_low)
+for i in range(age):
+    print(i)
+    if 0 >= i:
+        breaths_low += (525600*25)
+        breaths_high += (525600*60)
         beats += (67.5*525600)
+        print(breaths_low, "0-1")
+    if 1< i+1 <=4 :
+        breaths_low += (525600*20)
+        breaths_high += (525600*30)
+        beats += (67.5*525600)
+        print(breaths_low, "1-4")
+    if 4< i+1 <14 :
+        breaths_low += (525600*15)
+        breaths_high += (525600*25)
+        beats += (67.5*525600)
+        print(breaths_low,"4-14")
+    if 14< i+1 <18 :
+        breaths_low += (525600*11)
+        breaths_high += (525600*18)
+        beats += (67.5*525600)
+        print(breaths_low,"14-18")
 
 print("you have breathed", breaths_low, "-", breaths_high, "times")
 print("heart beated", beats, "times")
