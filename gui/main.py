b122db517c070e211e73a7a54664f3d5d9e4cd5b
Earth13wells <gabrielw.presley@protonmail.com>
2021-02-19 20:11:33 -0800

    life signs gui

--- gui/main.py
+++ gui/main.py
@@ -51,9 +51,44 @@ nameEntered.grid(column = 2, row = 1, padx = 30, pady = 0)
 tk.Button(tab1, text="Click Me", command= homeLoan, width = 10).grid(column = 1, row = 2, padx = 0, pady = 0)
 
 
+# life Signs
+age = tk.IntVar()
+
+def lifeSigns():
+    aged = age.get() + 1
+    breaths_low = breaths_high = beats = 0
+
+    print(breaths_low)
+    for i in range(aged):
+        if 0 >= i:
+            breaths_low += (525600*25)
+            breaths_high += (525600*60)
+            beats += (67.5*525600)
+        if 1< i+1 <=4 :
+            breaths_low += (525600*20)
+            breaths_high += (525600*30)
+            beats += (67.5*525600)
+        if 4< i+1 <14 :
+            breaths_low += (525600*15)
+            breaths_high += (525600*25)
+            beats += (67.5*525600)
+        if 14< i+1 <18 :
+            breaths_low += (525600*11)
+            breaths_high += (525600*18)
+            beats += (67.5*525600)
+    outputbreaths = 'you have breathed:\n' + str(breaths_low) + "-" + str(breaths_high) + '\ntimes!'
+    outputbeats = "Heart Beated:\n" + str(beats) + "\ntimes!"
+    ttk.Label(tab2, text = outputbreaths).grid(column = 0, row = 2, padx = 30, pady = 0)
+    ttk.Label(tab2, text = outputbeats).grid(column = 2, row = 2, padx = 30, pady = 0)
+
+ttk.Label(tab2, text ="").grid(column = 0, padx = 90) # just a spacer
+ttk.Label(tab2, text ="Age 0-18").grid(column = 1, row = 0, padx = 30, pady = 0)
+nameEntered = ttk.Entry(tab2, width = 15, textvariable = age)
+nameEntered.grid(column = 1, row = 1, padx = 30, pady = 0)
+
+tk.Button(tab2, text="Click Me", command= lifeSigns, width = 10).grid(column = 1, row = 2, padx = 0, pady = 0)
 
 
-# ttk.Label(tab2, text ="tab2").grid(column = 0, row = 0, padx = 30, pady = 30)
 # ttk.Label(tab3, text ="tab3").grid(column = 0, row = 0, padx = 30, pady = 30)
 # ttk.Label(tab4, text ="tab4").grid(column = 0, row = 0, padx = 30, pady = 30)
 # ttk.Label(tab5, text ="tab5").grid(column = 0, row = 0, padx = 30, pady = 30)
